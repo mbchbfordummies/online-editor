584705b66a3414cf5d7fcb1848fb04d68fd5bc70
Emmanuel <emmanuelqkorir@gmail.com>
2023-09-15 15:02:08 +0300

    add template.php

---
+++ template.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/main.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>MBChB for Dummies Editor</title>
+</head>
+
+<body class="bg-gray-100 overflow-auto">
+<nav class="p-4">
+        <div class=" container mx-auto flex justify-between items-center">
+<!-- logo on the left -->
+<div>
+    <a href="index.php" class=""><img src="public/dummies_logo.png" alt="logo" class="w-12 h-auto"></a>
+</div>
+
+<!-- menu items on the right -->
+    <ul class="flex justify-end">
+        <li class="mr-6"><a href="index.php" class="text-cyan-700 hover:text-cyan-950">Help</a></li>
+        <li class="mr-6"><a href="contact.php" class="text-cyan-700 hover:text-cyan-950">Contact</a></li>
+    </ul>
+        </div>
+    </nav>
+    <div class="flex flex-col justify-center items-center my-1">
+
+
+
+
+</body>
